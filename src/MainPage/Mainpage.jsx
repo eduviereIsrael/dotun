e29af53376fc654cc0203f6db8fc6447165d0b36
Eduviere Israel <eduviereisrael@gmail.com>
2022-07-26 04:28:11 +0200

    styling

--- src/MainPage/Mainpage.jsx
+++ src/MainPage/Mainpage.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './mainpage.css'
-import Hero from './sections/Hero'
-import Marquee from './sections/Marquee'
+import { Hero, Marquee, Works } from './sections'
 
 const Mainpage = () => {
   return (
     <div className='mainpage'>
       <Hero />
       <Marquee />
+      <Works />
     </div>
   )
 }
---
+++ src/MainPage/sections/index.js
@@ -0,0 +1,3 @@
+export {default as Hero} from './Hero';
+export {default as Marquee} from './Marquee';
+export {default as Works} from './Works';
