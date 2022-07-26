d7d257f15e4ec424bcdc9aedc6d296010a33cda0
Eduviere Israel <eduviereisrael@gmail.com>
2022-07-26 06:02:57 +0200

    styling

--- src/MainPage/sections/Works.jsx
+++ src/MainPage/sections/Works.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <div className='works'>
+        <div className='intro'>
+            <h2 className='gradient'>Projects</h2>
+            <p>Top curated products I have worked on</p>
+        </div>
+        <div className='works-container'>
+            
+        </div>
+        
+    </div>
   )
 }
 
