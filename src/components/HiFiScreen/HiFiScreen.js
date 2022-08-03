6424d667d094040396029378c2e38ad893fd53f8
Eduviere Israel <eduviereisrael@gmail.com>
2022-08-03 02:07:32 +0200

    updated img/div carousel

--- src/components/HiFiScreen/HiFiScreen.js
+++ src/components/HiFiScreen/HiFiScreen.js
@@ -4,7 +4,7 @@ import './hifiscreen.css'
 const HiFiScreen = (props) => {
   return (
     <div className='hifi-screen'>
-        <img src={props.img} alt={props.img} />
+        <img src={props.img} alt={props.img} style={{width: '240px'}}/>
         <p className='small-text'>{props.screenName}</p>
     </div>
   )
